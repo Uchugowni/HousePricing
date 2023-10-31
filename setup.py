24775434e1504edab8d098b99ec5ada588e8dfa1
Giribabu <Uchugowni@users.noreply.github.com>
2023-10-31 12:07:36 +0530

    new Setup

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import find_packages, setup
-#import setuptools 
-
-#with open("README.md", "r", encoding="utf-8") as f:
-#    long_description = f.read()
-
+from typing import List
 
 __version__="0.0.0"
 
 REPO_NAME = "HousePricing"    #github repository name
 AUTHOR_USER_NAME = "Uchugowni"  #github name
-SRC_REPO = "housePricing"   #src\housePricing under src repo
 AUTHOR_EMAIL = "uchugownigiribabu@gmail.com" #github email id
 
 HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
@@ -26,16 +21,10 @@ def get_requirements(file_path:str)->list[str]:
 
 
 setup(
-    name=SRC_REPO,
+    name=REPO_NAME,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for NLP app",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"":"src"},
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
