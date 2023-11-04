73c7a40a635fcd9021f65ac43f2b48fb3d474906
Giribabu <Uchugowni@users.noreply.github.com>
2023-11-04 18:31:54 +0530

    Practice

--- src/utils.py
+++ src/utils.py
@@ -45,6 +45,27 @@ def evaluate_models(x_train, y_train, x_test, y_test, models, param):
     
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_model(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+
+            model.fit(x_train, y_train)
+
+            Y_train_pred = model.predict(x_train)
+            Y_test_pred = model.predict(x_test)
+
+            train_model_r2_score = r2_score(y_train, Y_train_pred)
+            test_model_r2_score = r2_score(y_test, Y_test_pred)
+            logging.info(f"modela name --> {model} trann-r2-score {train_model_r2_score} and test_model_r2_score {test_model_r2_score}")
+
+            report[list(models.keys())[i]]=test_model_r2_score
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
 
 
 def load_object(file_path):
